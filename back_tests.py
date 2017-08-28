f0286b9dcecddd3f866868847b96555995b7432c
Astaroth <psljp@protonmail.com>
2017-08-28 16:20:52 -0300

    added classes tests

--- back_tests.py
+++ back_tests.py
@@ -1,5 +1,5 @@
+import classes
 import funciones
-import os
 import subprocess
 import unittest
 
@@ -11,6 +11,7 @@ class TestBack(unittest.TestCase):
         subprocess.call(["touch", "testdir2/testfile"])
         subprocess.call(['touch', '-m', '-d', "8 days ago", 'testdir3/testfile'])
         subprocess.call(['touch', '-m', '-d', "2 days ago", 'testdir3/testfile1'])
+        self.td = classes.TimedDirectory("testdir3")
 
     def test_empty_dir(self):
         res = funciones.older_than_list("testdir/", 666)
@@ -23,6 +24,18 @@ class TestBack(unittest.TestCase):
     def test_older_and_newer_files(self):
         res = funciones.older_than_list("testdir3/", 7)
         self.assertEqual(res, ["testdir3/testfile"])
+
+    def test_oldest_files_list(self):
+        res = self.td.oldest_files_list(4)
+        res1 = self.td.oldest_files_list(1)
+        res2 = self.td.oldest_files_list(100)
+        self.assertEqual(res, ["testdir3/testfile"])
+        self.assertEqual(res1, ["testdir3/testfile", "testdir3/testfile1"])
+        self.assertEqual(res2, [])
+
+    def test_newest_file_older_than(self):
+        self.assertTrue(self.td.newest_file_older_than(1))
+        self.assertFalse(self.td.newest_file_older_than(100))
 
     @classmethod
     def tearDownClass(self):
